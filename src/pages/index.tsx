ee752d246f86b3eade638f0257b97932596fca06
pallomagualter <pallomagualter@gmail.com>
2021-03-03 17:10:41 -0300

    Document

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+        
       </head>
 
     <ExperienceBar />
